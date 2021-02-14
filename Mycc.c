7b431054351963fc695febe1f649ab3e1c361bff
stonelike <f.r2christ@gmail.com>
2021-02-14 12:25:32 +0900

    step4

--- Mycc.c
+++ Mycc.c
@@ -25,6 +25,22 @@ struct Token
 //現在着目しているトークン
 Token *token;
 
+char *user_input;
+
+void error_at(char *loc, char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input; //アドレスの場所の差をとる,例えばアドレス400がLocで390がuser_inputだったら10が出てくる
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 //error報告用関数
 void error(char *fmt, ...)
 {
@@ -48,14 +64,14 @@ void expect(char op)
     // 次のトークンが期待している記号のときには、トークンを1つ読み進める。
     // それ以外の場合にはエラーを報告する。
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -75,8 +91,11 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
     return tok;
 }
 
-Token *tokenize(char *p)
+Token *tokenize()
 {
+
+    char *p = user_input;
+
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -102,7 +121,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -117,7 +136,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    token = tokenize(argv[1]); //TokList作成
+    user_input = argv[1];
+
+    token = tokenize(); //TokList作成
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
@@ -131,12 +152,12 @@ int main(int argc, char **argv)
     while (!at_eof())
     {
         if (consume('+'))
-        {
+        { //+の次はnumberが来てほしい
             printf("   add rax, %d\n", expect_number());
             continue;
         }
 
-        expect('-');
+        expect('-'); //-1もそう
         printf("   sub rax, %d\n", expect_number());
     }
 
